6615b734d010dfe76badcfca765c346caf241907
Uddin <mduddin@ad.helsinki.fi>
2020-11-08 12:41:21 +0200

    2nd Exercise

---
+++ Data/create_learning2014.R
@@ -0,0 +1,96 @@
+# Md Karim Uddin, 02.11.2020, 
+#Exercise 2
+
+
+# read the data into memory
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+# Look at the dimensions of the data
+
+dim(lrn14)
+
+# Look at the structure of the data
+str(lrn14)
+
+#lrn14 is available
+
+# divide each number in a vector
+c(1,2,3,4,5) / 2
+
+# print the "Attitude" column vector of the lrn14 data
+lrn14$Attitude
+
+# divide each number in the column vector
+lrn14$Attitude / 10
+
+# create column "attitude" by scaling the column "Attitude"
+lrn14$attitude <- lrn14$Attitude / 10
+
+# Access the dplyr library
+library(dplyr)
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+# access the dplyr library
+library(dplyr)
+
+# choose a handful of columns to keep
+keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
+
+# select the 'keep_columns' to create a new dataset
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+# see the stucture of the new dataset
+
+str(learning2014)
+
+
+# print out the column names of the data
+colnames(learning2014)
+
+# change the name of the second column
+colnames(learning2014)[2] <- "age"
+
+# change the name of "Points" to "points"
+colnames(learning2014)[7] <- "points"
+
+# print out the new column names of the data
+colnames(learning2014)
+
+# learning2014 is available
+
+# access the dplyr library
+library(dplyr)
+
+# select male students
+male_students <- filter(learning2014, gender == "M")
+
+# select rows where Points is greater than zero
+learning2014 <- filter(learning2014, points > 0)
+
+write.table(learning2014, file = "learning2014.txt", sep="\t")
+read.table( "learning2014.txt", header = TRUE,sep = "\t", stringsAsFactors = FALSE)
+str(learning2014)
+write.table(learning2014, file = "learning2014.csv", sep=",")
+
+library(GGally)
+library(ggplot2)
+
+library(knitr)
+library(rmarkdown)
+devtools::session_info()
